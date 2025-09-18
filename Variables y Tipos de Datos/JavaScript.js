9b77303695b4e19e8abab72bf55db099ae430c2c
Sebastian Daza <sebastiandaza0209@gmail.com>
2025-09-18 17:53:00 -0500

    Variables y Tipos de Datos

---
+++ Variables y Tipos de Datos/JavaScript.js
@@ -0,0 +1,11 @@
+let saludar = "Hola mundo" // String
+console.log(saludar);
+console.log(typeof saludar);
+
+let num1 = 2 // Number
+console.log(num1);
+console.log(typeof num1);
+
+let v = true // Boolean
+console.log(v);
+console.log(typeof v);
---
+++ Variables y Tipos de Datos/Python.py
@@ -0,0 +1,11 @@
+name = "Messi" # String
+print(name)
+print(type(name))
+
+número = 25 # Number
+print(número)
+print(type(número))
+
+boolean = True # Boolean
+print(boolean)
+print(type(boolean))
