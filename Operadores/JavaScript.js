d0c2c0b88f1a97cd4122e419620c689d9dba04c9
Sebastian Daza <sebastiandaza0209@gmail.com>
2025-09-18 19:12:05 -0500

    Operadores

---
+++ Operadores/JavaScript.js
@@ -0,0 +1,46 @@
+// Luka
+
+// Suma
+let Num1 = 2
+let Num2 = 3
+let suma1 = Num1 + Num2
+console.log("La suma de los 2 numeros es de: " + suma1);
+
+//resta 
+let na = 5
+let nb = 3
+let resta = na - nb
+console.log("La resta de los 2 numeros es de: " + resta);
+
+console.log("---------------");
+
+
+// Andres
+
+// Suma
+let num1 = 3;
+let num2 = 4;
+let sum = num1 + num2;
+console.log("El resultado de la suma es: " + sum);
+
+console.log("---------------");
+
+// Jennifer
+
+// Suma
+let num3 = 15;
+let num4 = 20;
+let suma2 = num3 + num4;
+console.log("La suma es: " + suma2);
+
+console.log("---------------");
+
+// Luciano
+let number1 = 5;
+let number2 = 10;
+
+let suma = number1 + number2;
+console.log("El resultado de la suma es: " + suma);
+
+let resta1 = number1 - number2;
+console.log("El resultado de la resta es: " + resta);
---
+++ Operadores/Python.py
@@ -0,0 +1,106 @@
+
+# Luka
+#suma 
+Num1 = 5
+Num2 = 6
+
+suma = Num1 + Num2 
+print("La suma de los 2 numeros es de: ", suma)
+
+res = Num1 - Num2 
+print("La resta de los 2 numeros es de: ", res)
+
+mul = Num1 * Num2
+print("La resta de los 2 numeros es de: ", mul)
+
+division1 = Num1 / Num2 
+print("La divicion de los 2 numeros es de: ", division1)
+
+modulo = Num1 % Num2 
+print("El modulo de los 2 numeros es de: ", modulo)
+
+#Aqui termina lo mio abraham (Luka)
+print("----------------")
+
+
+
+# -------------------------------------------------------
+
+# Andres
+numero1_Smoke = 29
+numero2_Smoke = 10
+numero3_Smoke = 1
+
+# suma
+suma_Smoke = (numero1_Smoke + numero2_Smoke) * numero3_Smoke
+# resta
+resta_Smoke = (numero1_Smoke - numero2_Smoke)
+# division
+div_Smoke = (numero1_Smoke / numero2_Smoke)
+# multiplicacion
+multiplicacion_Smoke = (numero1_Smoke * numero2_Smoke)
+# modulo
+mod_Smoke = (numero1_Smoke % numero2_Smoke)
+
+# mostramos el mensaje
+print("La suma es:", suma_Smoke)
+print("La resta es:", resta_Smoke)
+print("La division es:", div_Smoke)
+print("El modulo es:", mod_Smoke)
+print("La multiplicacion es:", multiplicacion_Smoke)
+print("----------------")
+
+# -------------------------------------------------------
+
+# Jennifer
+
+# Declaracion de variables
+x = 15
+y = 20
+
+# Suma
+suma = x + y
+print("La suma es:", suma)
+
+# Resta
+resta = x - y
+print("La resta es:", resta)
+
+# Multiplicacion
+multi = x * y 
+print("La multiplicacion es:", multi)
+
+# Division 
+division = x / y
+print("La division es:", division)
+
+
+# Luciano
+numero1 = 20
+numero2 = 15
+
+suma = numero1 + numero2
+resta = numero1 + numero2
+print("----------------")
+
+# -------------------------------------------------------
+
+# Luciano
+numero1 = 20
+numero2 = 15
+
+suma = numero1 + numero2
+print("El resulatdo es:", suma)
+
+resta = numero1 - numero2
+print("El resultado es:", resta)
+
+multiplicacion = numero1 * numero2
+print("El resultado es:", multiplicacion)
+
+divicion = numero1 / numero2
+print("El resultado es:", divicion)
+
+modulo = numero1 % numero2
+print("El resultado es:", modulo)
+print("----------------")
