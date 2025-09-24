eca246d60dd231486a5ea531c0741bd6235bc327
Sebastian Daza <sebastiandaza0209@gmail.com>
2025-09-24 11:57:26 -0500

    Variables y Tipos de Datos

---
+++ [01]Variables y Tipos de Datos/JavaScript.js
@@ -0,0 +1,38 @@
+// COMENTARIOS
+
+// Este es un comentario en una linea
+
+/*
+Este es un
+comentario
+de verias lineas
+*/
+
+// Variables
+
+var variable1 = 'Hola' // Metodo Antiguo y No Recomendado
+// Cambiar el Valor
+variable1 = 'Hello' // Se Puede
+
+let Variable2 = 'Como'
+// Cambiar el Valor
+Variable2 = 'How' // Se Puede
+
+const variable3 = 'Estan'
+// Cambiar el Valor
+// variable 3 = 'Are You' (NO SE PUEDE)
+
+// Tipos de Datos Principales
+
+let string = 'Sebastian'
+console.log(string);
+console.log(typeof string);
+
+
+let number = 18
+console.log(number);
+console.log(typeof number);
+
+let boolean = false
+console.log(boolean);
+console.log(typeof boolean);
---
+++ [01]Variables y Tipos de Datos/Python.py
@@ -0,0 +1,24 @@
+# COMENTARIOS
+
+# Este es un comentario en una linea
+
+'''
+Este es un
+comentario
+de verias lineas
+'''
+
+# Variables
+
+Variable1 = 'Hola mundo'
+# Cambiar el valor
+Variable1 = 'Hello World' # Se Puede
+
+string = 'Sebastian'
+print(string)
+
+number = 18
+print(number)
+
+boolean = False
+print(boolean)
